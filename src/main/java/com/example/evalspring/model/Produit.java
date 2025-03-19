437e6074f31fe8abadee07cb7e35cd00014b5d58
yassine <yassinesebaihia1@gmail.com>
2025-03-19 11:12:08 +0100

    feat: partie n°1

---
+++ src/main/java/com/example/evalspring/model/Produit.java
@@ -0,0 +1,59 @@
+package com.example.evalspring.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "evalspring")
+public class Produit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nom",nullable = false,length = 50)
+    @NotBlank(message = "Le champs doit être rempli")
+    @Min(value = 2,message = "Le nom doit avoir plus de 2 caractères")
+    private String nom;
+
+    @Column(name = "prix",nullable = false )
+    @NotBlank(message = "Le prix doit être renseigné")
+    @DecimalMin(value = "0.01",message = "Le prix doit être supérieur à 0 €")
+    private double prix;
+
+    //Constructeurs :
+    public Produit() {}
+    public Produit(String nom, double prix) {
+        this.nom = nom;
+        this.prix = prix;
+    }
+    // Getters et Setters :
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public double getPrix() {
+        return prix;
+    }
+
+    public void setPrix(double prix) {
+        this.prix = prix;
+    }
+}
+

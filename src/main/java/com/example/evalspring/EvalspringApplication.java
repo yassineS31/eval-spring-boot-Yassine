be574512d8d48cf417ec29da7d1d18339f7aadc0
yassine <yassinesebaihia1@gmail.com>
2025-03-19 10:10:23 +0100

    initialize project

---
+++ src/main/java/com/example/evalspring/EvalspringApplication.java
@@ -0,0 +1,13 @@
+package com.example.evalspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvalspringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EvalspringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/evalspring/EvalspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.evalspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvalspringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
